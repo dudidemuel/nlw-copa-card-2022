cb14d41906b5d518652f1aee3eb6ea816b0f9d20
José Manuel <101150563+dudidemuel@users.noreply.github.com>
2022-10-31 14:22:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let isExplorer = true
+
+function changeCard(event) {
+  const card = event.currentTarget
+  const bg = isExplorer 
+    ? "ignite"
+    : "explorer"
+
+  isExplorer = !isExplorer
+  
+  card.style.backgroundImage = `url(./assets/bg-${bg}.svg`
+}
